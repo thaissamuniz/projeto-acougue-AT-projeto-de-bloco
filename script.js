e8e6e0f98c2c73ae9d57fe00b7d3e5e6048cbdc7
thaissa <thaiissa.muniiz@gmail.com>
2022-12-16 18:43:57 -0300

    add: banner

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const openBtnEl = document.querySelector(".menu__open");
 const closeBtnEl = document.querySelector(".menu__close");
 const frtsTitle = document.querySelector('.banner__title--one');
 const scndTitle = document.querySelector('.banner__title--two');
-
+const banner = document.querySelector('.banner');
+let currentBanner = 0;
 
 openBtnEl.addEventListener('click', () => {
     document.documentElement.classList.add('menu__active');
@@ -18,9 +19,34 @@ document.documentElement.addEventListener('click', (e) => {
     }
 });
 
-const changeTitle = () => {
-    frtsTitle.classList.add('hidden-title');
-    scndTitle.classList.remove('hidden-title');
+var banners = [{
+    img: "img/AdobeStock_341409259.jpeg",
+    title: "Cortes para churrasco Uruguaios e Argentinos"
+},
+{
+    img: "https://images.pexels.com/photos/2647939/pexels-photo-2647939.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Encomendas de ostras vivas de SC"
+}
+];
+
+
+
+const changeBanner = () => {
+    currentBanner = (currentBanner + 1) % 2;
+    banner.style.backgroundImage = `url(${banners[currentBanner].img})`;
+    document.querySelector('.banner__title').innerText = `${banners[currentBanner].title}`;
 }
 
-setTimeout(changeTitle, 5000);
+timer = setInterval(changeBanner, 3000);
+
+banner.addEventListener('mouseover', e => {
+    if (e) {
+        clearInterval(timer);
+    }
+});
+
+banner.addEventListener('mouseout', e => {
+    if (e) {
+        timer = setInterval(changeBanner, 3000);
+    }
+});
